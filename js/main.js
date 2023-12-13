547dd5ebb4f9ced557731002df3b8bee46ff6247
Imronshoh <monikapi2022@gmail.com>
2023-12-13 20:47:47 +0500

    sd

--- js/main.js
+++ js/main.js
@@ -4,34 +4,31 @@ function fn() {
   var on = document.querySelector(".on");
   switch (Math.floor(elInp.value / 10)) {
     case 1:
-      on.textContent = "on";
+      on.textContent = "on-";
       break;
     case 2:
-      on.textContent = "yigirma";
+      on.textContent = "yigirma-";
       break;
     case 3:
-      on.textContent = "ottiz";
+      on.textContent = "ottiz-";
       break;
     case 4:
-      on.textContent = "qiriq";
+      on.textContent = "qiriq-";
       break;
     case 5:
-      on.textContent = "ellik";
+      on.textContent = "ellik-";
       break;
     case 6:
-      on.textContent = "oltmish";
+      on.textContent = "oltmish-";
       break;
     case 7:
-      on.textContent = "yetmish";
+      on.textContent = "yetmish-";
       break;
     case 8:
-      on.textContent = "sakson";
+      on.textContent = "sakson-";
       break;
     case 9:
-      on.textContent = "toqson";
-      break;
-    default:
-      on.textContent = "Not a Number (NaN)";
+      on.textContent = "toqson-";
       break;
   }
   var bir = document.querySelector(".bir");
@@ -41,31 +38,31 @@ function fn() {
       bir.textContent = "";
       break;
     case 1:
-      bir.textContent = "-bir";
+      bir.textContent = "bir";
       break;
     case 2:
-      bir.textContent = "-ikki";
+      bir.textContent = "ikki";
       break;
     case 3:
-      bir.textContent = "-uch";
+      bir.textContent = "uch";
       break;
     case 4:
-      bir.textContent = "-tort";
+      bir.textContent = "tort";
       break;
     case 5:
-      bir.textContent = "-besh";
+      bir.textContent = "besh";
       break;
     case 6:
-      bir.textContent = "-olti";
+      bir.textContent = "olti";
       break;
     case 7:
-      bir.textContent = "-yetti";
+      bir.textContent = "yetti";
       break;
     case 8:
-      bir.textContent = "-sakkiz";
+      bir.textContent = "sakkiz";
       break;
     case 9:
-      bir.textContent = "-toqqiz";
+      bir.textContent = "toqqiz";
       break;
     default:
       on.textContent = "Not a Number (NaN)";
